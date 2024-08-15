f8fb1f1f02e8563d2f187cc146b780949072623d
JSM788 <salinasjuan788@gmail.com>
2024-08-14 20:29:45 -0500

    Uploading Examen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,84 @@
+import { useState } from "react";
+import Card from "./Card";
+
 function App() {
-  //Aqui deberias agregar los estados y los handlers para los inputs
+  const [showCard, setShowCard] = useState(false);
+  const [error, setError] = useState("");
+
+  const [form, setForm] = useState({
+    nombre: "",
+    color: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (validateForm()) {
+      setShowCard(true);
+      setError("");
+    } else {
+      setShowCard(false);
+    }
+  };
+
+  const validateForm = () => {
+    let newError = "";
+
+    if (form.nombre.trim().length < 3 || form.nombre.trim().startsWith(" ")) {
+      newError = "Por favor chequea que la información sea correcta";
+    }
+
+    const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+    if (!hexColorRegex.test(form.color)) {
+      newError = "Por favor chequea que la información sea correcta";
+    }
+
+    setError(newError);
+    return !newError;
+  };
+
+  const handleForm = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   return (
     <div className="App">
       <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="nombre">Nombre: </label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            placeholder="Ingresa tu nombre"
+            onChange={handleForm}
+            value={form.nombre}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="color">Color: </label>
+          <input
+            type="text"
+            id="color"
+            name="color"
+            placeholder="Ingresa tu color favorito (hex)"
+            onChange={handleForm}
+            value={form.color}
+          />
+        </div>
+        {error && <small style={{ color: "red" }}>{error}</small>}
+
+        <div className="form-group">
+          <button type="submit" className="submit-button">
+            Enviar
+          </button>
+        </div>
+      </form>
+      {showCard && <Card form={form} />}
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,18 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card({ form }) {
   return (
-    <div>
-      <h2>Esto es un componente</h2>
+    <div className="card">
+      <h4>Hola {form.nombre}!</h4>
+      <p>
+        <strong>Sabemos que tu color favorito es: </strong>
+      </p>
+      <div style={{ backgroundColor: form.color }} className="card-color">
+        {form.color}
+      </div>
     </div>
-  );
+  )
 }
 
-export default Card;
+export default Card
